a94aa0111373a86b8e89f30224c354ffcc60f9f6
skullwolf <kulangzuzhangvvip@163.com>
2022-05-26 20:05:00 +0800

    init

---
+++ devops-demo/src/main/java/com/itdachang/devopsdemo/DevopsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.itdachang.devopsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsDemoApplication.class, args);
+    }
+
+}
